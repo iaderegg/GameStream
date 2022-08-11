656ca178687f3e43c1847455853f65192fe9d5ca
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-08-11 18:41:42 -0500

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Iader Emir Garcia Gomez on 11/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Iader Emir Garcia Gomez on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
