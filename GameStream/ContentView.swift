bd9b290fc26ccd4b7eed4e5f361894bf37a4effa
Iader E. García Gómez <iadergg@gmail.com>
2022-08-19 23:43:51 -0500

    First commit

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,13 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            
+            Spacer ()
+            
+            Color("Background").ignoresSafeArea()
+            
+            VStack {
+                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 42)
+                
+                InicioYRegistroView()
+            }
+        }
+    }
+}
+
+struct InicioYRegistroView:View {
+    
+    @State var tipoInicioSesion = true
+    
+    var body: some View {
+        VStack {
+            HStack {
+                
+                Spacer()
+                
+                Button("INICIA SESIÓN") {
+                    tipoInicioSesion = true
+                }.foregroundColor(tipoInicioSesion ? .white : .gray)
+                
+                Spacer()
+                
+                Button("REGÍSTRATE") {
+                    tipoInicioSesion = false
+                }.foregroundColor(tipoInicioSesion ? .gray : .white)
+                
+                Spacer ()
+            }
+            
+            Spacer(minLength: 42)
+            
+            if tipoInicioSesion == true {
+                InicioSesionView()
+            } else {
+                RegistroView()
+            }
+        }
+    }
+}
+
+struct InicioSesionView:View {
+    var body: some View {
+        
+        ScrollView {
+            VStack {
+                Text("Correo Electrónico").foregroundColor(Color("Dark-cian"))
+            }
+        }
+    }
+}
+
+struct RegistroView:View {
+    var body: some View {
+        Text("Soy la vista de registro").foregroundColor(.white)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("pantalla01").resizable()
         ContentView()
     }
 }
